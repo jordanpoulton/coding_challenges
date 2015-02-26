88d4ba764424ca0de3c5cb09485f64689d511735
Joseph Knowles <joejknowles@gmail.com>
2015-02-26 15:14:36 +0000

    Added Mark and Joe's solution to longest palindrome

--- set_1/solutions/17-longest-palindrome.rb
+++ set_1/solutions/17-longest-palindrome.rb
@@ -34,3 +34,31 @@ def longest_palindrome(string)
   return best_palindrome
 end
 #===========
+#Mark n Joe
+def palindrome?(string)
+  i = 0
+  while i < string.length
+    if string[i] != string[(string.length - 1) - i]
+      return false
+    end
+
+    i += 1
+  end
+
+  return true
+end
+
+def longest_palindrome(string)
+  pals = []
+  (0..string.length).each do |i|
+    (0..string.length - i).each do |l|
+      sub = string.slice(i,l)
+      if palindrome?(sub)
+        pals.push(sub)
+      end
+
+    end
+  end
+  pals.sort_by{|pal| pal.length}[-1]
+end
+#================================
