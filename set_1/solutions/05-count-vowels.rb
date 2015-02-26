856d326230ce272b9ebb71c63f421c5402630f16
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-26 12:10:46 +0000

    Completed 05-count-vowels.rb

--- set_1/solutions/05-count-vowels.rb
+++ set_1/solutions/05-count-vowels.rb
@@ -1,19 +1,56 @@
-#Jordan
+# Write a method that takes a string and returns the number of vowels
+# in the string. You may assume that all the letters are lower cased.
+# You can treat "y" as a consonant.
+#
+# Difficulty: easy.
+
 def count_vowels(string)
-  num_vowels = 0
-
-  i = 0
-  while i < string.length
-    if (string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u")
-      num_vowels += 1
+  vowels= "aeiou"
+  vowel_count = 0
+  string.chars do |ch|
+    vowels.chars do |vowel|
+      if ch == vowel
+        vowel_count += 1
+        break
+      end
     end
-
-    i += 1
   end
-
-  return num_vowels
+  vowel_count
 end
-#===========
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('count_vowels("abcd") == 1: ' + (count_vowels('abcd') == 1).to_s)
+puts('count_vowels("color") == 2: ' + (count_vowels('color') == 2).to_s)
+puts('count_vowels("colour") == 3: ' + (count_vowels('colour') == 3).to_s)
+puts('count_vowels("cecilia") == 4: ' + (count_vowels('cecilia') == 4).to_s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Mark & Joe
 def count_vowels(string)
   string = string.split('')
@@ -25,4 +62,3 @@ def count_vowels(string)
   end
   count
 end
-#=======
