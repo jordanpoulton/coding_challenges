090516e02a3907afe3c478432d1844cd07003d5c
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-02-26 13:37:15 +0200

    Completed challenge 04-time-conversion.rb

--- set_1/solutions/04-time-conversion.rb
+++ set_1/solutions/04-time-conversion.rb
@@ -1,29 +1,16 @@
-#Jordan
+# Write a method that will take in a number of minutes, and returns a
+# string that formats the number into `hours:minutes`.
+#
+# Difficulty: easy.
+
 def time_conversion(minutes)
-  hours = 0
-
-  while minutes >= 60
-    hours += 1
-    minutes -= 60
-  end
-
-  if minutes < 10
-    minutes_s = "0" + minutes.to_s
-  else
-    minutes_s = minutes.to_s
-  end
-
-  return hours.to_s + ":" + minutes_s
-end
-#===========
-
-# Joe & Mark
-def time_conversion(minutes)
-  hours = minutes / 60
-  minutes = minutes % 60
-  if minutes < 10
-    minutes = "0#{minutes}"
-  end
-  "#{hours}:#{minutes}"
+  minutes%60 < 10 ? "#{minutes/60}:0#{minutes%60}" : "#{minutes/60}:#{minutes%60}"
 end
 
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('time_conversion(15) == "0:15": ' + (time_conversion(15) == '0:15').to_s)
+puts('time_conversion(150) == "2:30": ' + (time_conversion(150) == '2:30').to_s)
+puts('time_conversion(360) == "6:00": ' + (time_conversion(360) == '6:00').to_s)
+puts('time_conversion(7) == "0:07": ' + (time_conversion(7) == '0:07').to_s)
