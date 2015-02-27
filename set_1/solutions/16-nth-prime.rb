4c57d7a273c57f6db978bb8b7e3df7a9a138c1f6
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-27 13:51:40 +0000

    Completed 16-nth-prime.rb

--- set_1/solutions/16-nth-prime.rb
+++ set_1/solutions/16-nth-prime.rb
@@ -32,7 +32,7 @@ def nth_prime(n)
     i += 1
   end
 end
-#===========
+
 # Joe & Mark
 def nth_prime(n)
   i = 0
@@ -45,7 +45,7 @@ def nth_prime(n)
   end
   return num - 1
 end
-#===========
+
 
 # Joe & Gus
 
@@ -61,3 +61,18 @@ def nth_prime(n)
 
   return pointer
 end
+
+# Jade & Dan
+
+def nth_prime(n)
+  i = 0
+  num = 1
+  while i < n
+    if is_prime?(num)
+      i += 1
+    end
+    num += 1
+  end
+  return num - 1
+end
+
